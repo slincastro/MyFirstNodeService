5fd24524a73b2f5a3dfe72df3e7e367259441b87
slin.castro <slin.castro@bayteq.com>
2016-09-29 09:31:39 -0500

    cambio en serviceio nodejs

--- Mongoose.js
+++ Mongoose.js
@@ -42,6 +42,12 @@ var bodyParser     =        require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.post('/login', function(req, res) {
 	
 	console.log(req.body);
@@ -55,7 +61,7 @@ var account;
 var entity = require('./Entity/Entities.js');
 
 app.post('/Autenticate',function(req,res){
-
+  
 	var user = req.body.user;
     var password = req.body.password;
 
