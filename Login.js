a4fb750e6ef82e12db8e2b3f8fd3a917a053c75f
Slin <Slingc007@gmail.com>
2016-10-17 23:56:39 -0500

    adicionando nuevos estilos a la vista

--- Login.js
+++ Login.js
@@ -7,7 +7,14 @@ var bodyParser     =        require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 app.post('/Autenticate',function(req,res){
